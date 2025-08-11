a9cd8d585a88a63e7b2fb8a3329ce72e1e5cffda
techizone <venkateshsap413@gmail.com>
2025-08-11 16:20:10 +0530

    Added Project files

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Kodekloud E-Commerce</title>
+        <title>SapSecOps E-Commerce</title>
 
         <!-- Favicon -->
         <link rel="icon" href="img/favicon.png" type="image/png" />
@@ -55,8 +55,8 @@
                             <span class="icon-bar"></span>
                             </button>
                             <a class="navbar-brand" href="index.html">
-                                <img src="img/logo.png" alt="">
-                                <img src="img/logo-2.png" alt="">
+                                <img src="img/SSO.png" alt="">
+                                <img src="img/SSO.png" alt="">
                             </a>
                         </div>
                     </div>
@@ -162,7 +162,7 @@
 
 
                 <div class="copy_right_area">
-                    <h4 class="copy_right">© Copyright 2019 Kodekloud Ecommerce | All Rights Reserved</h4>
+                    <h4 class="copy_right">© Copyright 2019 SapSecOps Ecommerce | All Rights Reserved</h4>
                 </div>
             </div>
         </footer>
